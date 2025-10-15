d772f2b07af640935490d4c15ac8d761ccbb118e
Oladisea <ladisea55@gmail.com>
2025-10-15 20:37:50 +0100

    feat: initial commit

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,113 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color primaryColor = Color(0xff9B7BFF);
+  static const Color secondaryColor = Color(0xffFFB900);
+  static const Color tertiaryColor = Color(0xffFF8902);
+  static const Color orange = Color(0xffAB882B);
+  static const Color orange100 = Color(0xffE4C77A);
+  static const Color modalColor = Color(0xff28A745);
+
+  static const Color gradientColor1 = Color(0xFF2E98E9);
+  static const Color gradientColor2 = Color(0xFF7F39FB);
+  static const Color kWhite = Color(0xFFFFFFFF);
+  static const Color kBlack = Color(0xFF000000);
+  static const Color backgroundColor = Color(0XFFE2EAF6);
+  static const Color primaryTextColor = Color(0XFF2C3D7A);
+  static const Color buttonColor = Color(0XFF7B3EFF);
+  static const Color iconColor1 = Color(0xffff713a);
+  static const Color iconColor2 = Color(0xff01b2ad);
+  static const Color kGrey = Color(0XFFEBEBEB);
+  //static const Color tertiaryColor = Color(0XFFF0F0F0);
+
+
+
+
+  //green
+  static const Color green300 = Color(0xff216F5A);
+  static const Color green100 = Color(0xff1D7C26);
+  //white
+  static const Color white = Color(0xffFFFFFF);
+  static const Color white300 = Color(0xffF1F1F1);
+  static const Color white400 = Color(0xffE1E1E1);
+  static const Color white500 = Color(0xffD4D4D4);
+  static const Color white100 = Color(0xffCCCCCC);
+  static const Color white200 = Color(0xffEBEBEB);
+  static const Color white900 = Color(0xffF7F7F7);
+  static const Color white800 = Color(0xffE8E8E8);
+
+
+
+
+  static const Color black = Color(0xff110F0B);
+  static const Color textFieldBackground = Color(0xffF8FBFA);
+  static const Color textFieldColor = Color(0xff969696);
+  static const Color black200 = Color(0xff444444);
+  static const Color pinCodeBorder = Color(0xff969696);
+  static const Color otpResend = Color(0xff01553E);
+  static const Color deepBlack = Color(0xff110F0B);
+
+  static const Color brightGrey = Color(0xffEDF2F1);
+  static const Color amber = Color(0xffFFB900);
+  static const Color rose = Color(0xffFD98A4);
+
+  static const Color buttonDisabledColor = Color(0xffD1D1D1);
+
+
+  static const Color transparent = Color(0x00000000);
+
+
+
+
+  //shadow
+  static const Color shadowColor1 = Color(0xff01291E);
+
+  //grey
+  static const Color grey50 = Color(0xffF3F6F5);
+  static const Color grey100 = Color(0xffEAEEED);
+  static const Color grey200 = Color(0xffECF2F0);
+  static const Color grey300 = Color(0xffE2E7E6);
+  static const Color grey400 = Color(0xffEDF2F1);
+  static const Color grey500 = Color(0xffF4F7F6);
+
+  static const Color grey600 = Color(0xffB8BEBC);
+  static const Color grey700 = Color(0xffF4F4F9);
+  static const Color grey800 = Color(0xffA5A5A5);
+  static const Color grey900 = Color(0xffD9D9D9);
+
+
+  //black
+  static const Color black50 = Color(0xff808080);
+  static const Color black100 = Color(0xff606060);
+  static const Color black600 = Color(0xff2C2C2C);
+  static const Color black300 = Color(0xff5C5C5C);
+  static const Color black400 = Color(0xff000B1B);
+  static const Color black500 = Color(0xff333333);
+
+  //blue
+  static const Color blue300 = Color(0xff82B3F7);
+  static const Color blue600 = Color(0xff3658C1);
+  static const Color blue200 = Color(0xff96BFF8);
+  static const Color blue900 = Color(0xff1D737C);
+  static const Color blue800 = Color(0xff1D3557);
+
+
+
+
+  //red
+  static const Color red = Color(0xffDC3444);
+  static const Color red900 = Color(0xff891C1C);
+  static const Color red500 = Color(0xffD80027);
+  static const Color red600 = Color(0xffEE191C);
+
+  static const Color red100 = Color(0xffFFE7EA);
+
+  //yellow
+  static const Color yellow = Color(0xffFBB347);
+  static const Color yellow900 = Color(0xffFF8902);
+  static const Color yellow100 = Color(0xffFFC72E);
+
+
+
+
+}
