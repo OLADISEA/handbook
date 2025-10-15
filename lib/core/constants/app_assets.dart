ac03cd4e587d4242d02e5c407619e25f0e2a7d34
Oladisea <ladisea55@gmail.com>
2025-10-15 20:37:23 +0100

    feat: initial commit

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,232 @@
+class AppAssets{
+  static const String onBoarding = 'assets/images/onBoarding-image.png';
+  static const String rider = 'assets/images/rider.png';
+  static const String textImage = 'assets/images/text-image.png';
+  static const String backArrow = 'assets/icons/sharp-arrow.png';
+  static const String arrowBack = 'assets/svg/arrow_back.svg';
+  static const String arrowDown = 'assets/svg/arrow_down.svg';
+  static const String fail = 'assets/svg/fail.svg';
+  static const String hotelComplete = 'assets/svg/hotel_complete.svg';
+  static const String maintenanceComplete = 'assets/svg/maintenance_complete.svg';
+  static const String barbingComplete = 'assets/svg/barbing_complete.svg';
+  static const String carComplete = 'assets/svg/car_complete.svg';
+  static const String ticket = 'assets/svg/ticket.svg';
+  static const String splash = 'assets/images/splash_img.png';
+
+  //nav bar
+  //static const String explore = 'assets/icons/explore.png';
+
+
+
+  static const String promoOff = 'assets/svg/promo-off.svg';
+
+  static const String calendar = 'assets/icons/calendar.png';
+  static const String placeholder = 'assets/images/placeholder.jpg';
+
+  static const String location = 'assets/svg/location.svg';
+  static const String locationBlack = 'assets/svg/location_black.svg';
+
+  static const String promo = 'assets/svg/promo.svg';
+  static const String promoIcon = 'assets/svg/promo_icon.svg';
+  static const String hotel = 'assets/svg/bed-svgrepo-com.svg';
+  static const String carRental = 'assets/svg/car-2-svgrepo-com.svg';
+  static const String maintenance = 'assets/svg/tools-svgrepo-com.svg';
+  static const String barbing = 'assets/svg/SVGRepo_iconCarrier.svg';
+  static const String restaurant = 'assets/svg/restaurant.svg';
+  static const String restaurantImg = 'assets/images/restaurant-img.jpg';
+  static const String pharmacy = 'assets/svg/pharmacies.svg';
+  static const String pharmacyImg = 'assets/images/pharmacy-img.jpg';
+  static const String supermarket = 'assets/svg/supermarket.svg';
+  static const String supermarketImg = 'assets/images/supermarket-img.jpg';
+  static const String service = 'assets/svg/services.svg';
+  static const String promoImg = 'assets/svg/promo-img.svg';
+  static const String republic = 'assets/svg/republic.svg';
+  static const String favorite = 'assets/svg/favorites.svg';
+  static const String rating = 'assets/svg/rating.svg';
+  static const String rate = 'assets/svg/rate.svg';
+  static const String accepted = 'assets/svg/accepted.svg';
+  static const String delivery = 'assets/svg/moped.svg';
+  static const String deliveryBlack = 'assets/svg/delivery_black.svg';
+
+  static const String offer = 'assets/svg/offers.svg';
+  static const String ventura = 'assets/svg/ventura.svg';
+  static const String ventura1 = 'assets/images/ventura.png';
+  static const String food = 'assets/images/food.jpg';
+  static const String pinnacle = 'assets/images/pinnacle.jpg';
+  static const String market = 'assets/images/market.png';
+  static const String search = 'assets/svg/search.svg';
+  static const String search1 = 'assets/images/search.png';
+  static const String favourite = 'assets/svg/hearts.svg';
+  static const String share = 'assets/svg/share.svg';
+  static const String share1 = 'assets/svg/share-img.svg';
+  static const String star = 'assets/svg/star.svg';
+  static const String shop = 'assets/svg/shop.svg';
+  static const String shop1 = 'assets/svg/shop-img.png';
+  static const String carRentals = 'assets/images/car_rentals.png';
+
+  static const String copy = 'assets/svg/copy.svg';
+  static const String delete = 'assets/svg/delete.svg';
+  static const String clear = 'assets/svg/clear.svg';
+  static const String wallet = 'assets/svg/wallet.svg';
+  static const String global = 'assets/svg/global.svg';
+  static const String back = 'assets/svg/back.svg';
+  static const String browse = 'assets/svg/browse.svg';
+  static const String coin = 'assets/svg/coin.svg';
+  static const String referrals = 'assets/svg/referrals.svg';
+  static const String referralImage = 'assets/images/referral_image.png';
+
+  static const String active = 'assets/svg/active.svg';
+  static const String point = 'assets/svg/point.svg';
+  static const String phone = 'assets/svg/phone.svg';
+  static const String note = 'assets/svg/note.svg';
+  static const String logOut = 'assets/svg/log_out.svg';
+  static const String support = 'assets/svg/support.svg';
+  static const String transaction = 'assets/svg/transaction.svg';
+  static const String transactions = 'assets/svg/transactions.svg';
+  static const String legal = 'assets/svg/legal.svg';
+  static const String promotion = 'assets/svg/promotion.svg';
+  static const String profile = 'assets/svg/profile.svg';
+  static const String referral = 'assets/svg/referral.svg';
+  static const String orderImg = 'assets/images/order.png';
+  static const String emptyBasket = 'assets/svg/empty-basket.svg';
+  static const String emptyBasket1 = 'assets/images/empty-basket.png';
+  static const String noOngoing = 'assets/svg/filled_basket.svg';
+  static const String noOngoing1 = 'assets/images/no-ongoing.png';
+  static const String completed = 'assets/images/completed.jpg';
+  static const String morningFresh = 'assets/images/morning_fresh.jpg';
+
+  static const String completed2 = 'assets/images/completed2.jpg';
+  static const String car = 'assets/images/car.jpg';
+  static const String faqImg = 'assets/images/faq-img.png';
+  static const String profileImg = 'assets/images/profile-img.png';
+  static const String carRent = 'assets/images/car_rent.png';
+  static const String rentals = 'assets/images/rentals.png';
+  static const String thanks = 'assets/images/thanks.png';
+  static const String pickUp = 'assets/images/pick_up.jpg';
+
+  static const String doodle = 'assets/svg/doodle.svg';
+  static const String doodle1 = 'assets/images/doodle.png';
+  static const String referralImg = 'assets/images/referral_img.png';
+  static const String hand = 'assets/images/hand.png';
+  static const String pizza = 'assets/svg/pizza.svg';
+  static const String burger = 'assets/svg/burger.svg';
+  static const String cheese = 'assets/svg/cheese.svg';
+  static const String ratingBag = 'assets/svg/rating-bag.svg';
+  static const String thumbsUp = 'assets/svg/thumbs-up.svg';
+  static const String time = 'assets/svg/time.svg';
+  static const String thumbsDown = 'assets/svg/thumbs-down.svg';
+  static const String bike = 'assets/svg/bike.svg';
+  static const String call = 'assets/svg/call.svg';
+  static const String hitMark = 'assets/svg/hitMark.svg';
+  static const String hitMarkPlace = 'assets/svg/hit_mark_primary.svg';
+
+  static const String meal = 'assets/svg/meal.svg';
+  static const String mail = 'assets/svg/mail.svg';
+  static const String mall = 'assets/svg/mall.svg';
+  static const String transit = 'assets/svg/transit.svg';
+  static const String feedback = 'assets/svg/feed_back.svg';
+  static const String whatsapp = 'assets/svg/whatsapp.svg';
+  static const String faq = 'assets/svg/faq.svg';
+  static const String arrowForward = 'assets/svg/arrow_right.svg';
+
+  //wallet
+  static const String pastTransaction = 'assets/svg/past_transaction.svg';
+  static const String masterCard = 'assets/svg/mc_symbol.svg';
+  static const String visa = 'assets/svg/visa-svgrepo-com.svg';
+
+  static const String serviceImg = 'assets/images/service-img.png';
+  static const String hotelImg = 'assets/images/hotel-img.png';
+
+
+
+
+
+  //nav bar icons
+  static const String explore = 'assets/svg/explore.svg';
+  static const String earth = 'assets/svg/earth.svg';
+  static const String cart = 'assets/svg/cart.svg';
+  static const String searchBar = 'assets/svg/search_bar.svg';
+  static const String account = 'assets/svg/account.svg';
+
+  //referrals
+  static const String bike2 = 'assets/svg/bike2.svg';
+  static const String promo2 = 'assets/svg/promo_off2.svg';
+  static const String promotion2 = 'assets/svg/promotion2.svg';
+
+  //terms and policy
+
+  static const String privacy = 'assets/svg/privacy.svg';
+  static const String terms = 'assets/svg/terms.svg';
+
+  //delete
+  static const String deleteImg = 'assets/images/delete_img.png';
+  static const String profileIcon = 'assets/svg/profile_grace.svg';
+  static const String walletAccount = 'assets/svg/wallet_account.svg';
+  static const String logs = 'assets/svg/logs.svg';
+  static const String favoriteWhite = 'assets/svg/favorite_white.svg';
+  static const String favoriteBlack = 'assets/svg/Favorite_black.svg';
+
+
+
+
+  static const String transactionEmpty = 'assets/svg/icon-park-twotone_tickets-two.svg';
+
+  //hotel
+  static const String ekoHotel = 'assets/images/eko_hotel.jpg';
+  static const String waterView = 'assets/images/water_view.jpg';
+  static const String ekoSuites = 'assets/images/eko_suites.jpg';
+
+
+
+
+
+  //room
+  static const String airCondition = 'assets/svg/ion_snow.svg';
+  static const String parking = 'assets/svg/mdi_parking.svg';
+  static const String pool = 'assets/svg/material-symbols_pool-rounded.svg';
+  static const String breakfast = 'assets/svg/mdi_free-breakfast.svg';
+  static const String wifi = 'assets/svg/material-symbols_wifi-rounded.svg';
+  static const String line = 'assets/svg/Line.svg';
+  static const String check = 'assets/svg/check.svg';
+
+  //barbing
+  static const String barbingService = 'assets/images/barbing_service.png';
+  static const String barbingBooking = 'assets/images/barbing_booking.png';
+  static const String barbingBooking2 = 'assets/images/barbing_booking2.png';
+  static const String barbingImg = 'assets/images/barbing_img.png';
+
+  //maintenance
+  static const String electricService = 'assets/images/electric_services.png';
+  static const String maintenanceService = 'assets/images/maintenance.png';
+  static const String plumbing = 'assets/images/plumbing.png';
+  static const String electricServiceBooking = 'assets/images/electricServiceBooking.png';
+
+
+
+  //pay online
+  static const String bank = 'assets/svg/bank.svg';
+  static const String transfer = 'assets/svg/transfer.svg';
+  static const String card = 'assets/svg/card_online.svg';
+  static const String ussd = 'assets/svg/ussd.svg';
+
+  //order complete
+  static const String orderAccepted = 'assets/svg/I21aTkwE0rUi8ijA9TJo7.svg';
+
+
+
+  //no internet connection
+  static const String noInternetConnection = 'assets/images/no_internet_connection.png';
+
+
+
+  // empty state
+  static const String searchEmpty = 'assets/svg/Frame.svg';
+  static const String promoEmpty = 'assets/svg/Frame985672.svg';
+
+
+
+
+
+
+
+}
