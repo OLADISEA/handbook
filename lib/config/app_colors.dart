d670962c5c0af97cd0ee62fb567eef7a6bf62d79
Oladisea <ladisea55@gmail.com>
2025-10-15 20:37:40 +0100

    feat: initial commit

---
+++ lib/config/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const Color gradientColor1 = Color(0xFF2E98E9);
+  static const Color gradientColor2 = Color(0xFF7F39FB);
+  static const Color kWhite = Color(0xFFFFFFFF);
+  static const Color kBlack = Color(0xFF000000);
+  static const Color backgroundColor = Color(0XFFE2EAF6);
+  static const Color primaryTextColor = Color(0XFF2C3D7A);
+  static const Color buttonColor = Color(0XFF7B3EFF);
+  static const Color iconColor1 = Color(0xffff713a);
+  static const Color iconColor2 = Color(0xff01b2ad);
+  static const Color kGrey = Color(0XFFEBEBEB);
+  static const Color tertiaryColor = Color(0XFFF0F0F0);
+
+
+
+
+}
