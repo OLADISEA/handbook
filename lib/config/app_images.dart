263e6875d83a49dd84f26f6eec685221131c36d7
Oladisea <ladisea55@gmail.com>
2025-10-31 23:08:11 +0100

    feat: add app images

---
+++ lib/config/app_images.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class AppImages{
+  static const String chatImage = 'assets/images/chat_image';
+  static const String appImage = 'assets/images/chat_image';
+  static const String settingsLogo = 'assets/images/chat_image';
+  static const String chatBackgroundImage = 'assets/images/chat_image';
+  static const String onBoardingImage = 'assets/images/chat_image';
+  static const String splashImage = 'assets/images/chat_image';
+  static const String profileImage = 'assets/images/chat_image';
+  static const String chatPageLogo = 'assets/images/chat_image';
+  static const String homePageLogo = 'assets/images/chat_image';
+
+}
