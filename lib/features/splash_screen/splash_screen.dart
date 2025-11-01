a05257730b952f1b72f30985d241821e59d6a4f4
Oladisea <ladisea55@gmail.com>
2025-11-01 23:00:42 +0100

    feat: design splash screen

---
+++ lib/features/splash_screen/splash_screen.dart
@@ -0,0 +1,103 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ui_chatbot/config/app_colors.dart';
+import 'package:ui_chatbot/core/widgets/texts/app_text.dart';
+
+import '../../core/constants/app_assets.dart';
+import '../../core/constants/app_colors.dart';
+import '../../widget/reusable_text.dart';
+import '../onboarding/onboarding_screen.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final Color blueColor = const Color(0xFF2E98E9);
+  final Color purpleColor = const Color(0xFF7F39FB);
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(milliseconds: 3000), () {
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) => const OnboardingPage()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [AppColor.gradientColor1, AppColor.gradientColor2],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                transform: GradientRotation(80 * pi / 180), // 80 degrees in radians
+              ),
+            ),
+          ),
+          Opacity(
+            opacity: 0.24,
+            child: ColorFiltered(
+              colorFilter: const ColorFilter.mode(
+                  AppColors.primaryColor,
+                  BlendMode.color
+              ),
+              child: Image.asset(
+                AppAssets.splash,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+          ),
+
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 300.h,),
+                Container(
+                  height: 200.h,
+                  width: 200.w,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Center(
+                    child: SizedBox(
+                      width: 180.h,
+                      child: Transform.rotate(
+                        angle: -30 * pi / 180,
+                        child: AppText(
+                          fontFamily: 'qurova',
+                          textAlign: TextAlign.center,
+                          text: "HandiBot",
+                          fontSize: 30.sp,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 280.h,),
+                Reusable(text: "A Platform for prospective UI students",textColor: Colors.white,fontSize: 12.sp,)
+              ],
+            ),
+
+        ],
+      ),
+    );
+  }
+}
