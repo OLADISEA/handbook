b9b0d8f9415d3026b2389740089c7acd27ba7b53
Oladisea <ladisea55@gmail.com>
2025-11-07 22:26:44 +0100

    feat: implement internet connectivity

---
+++ lib/bloc/connectivity/connectivity_state.dart
@@ -0,0 +1,4 @@
+abstract class ConnectivityState {}
+class ConnectivityInitial extends ConnectivityState {}
+class ConnectivityOnline extends ConnectivityState {}
+class ConnectivityOffline extends ConnectivityState {}
