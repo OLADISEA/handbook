9aeebc6f9340e2bd2d139276531062708921b878
Oladisea <ladisea55@gmail.com>
2025-11-08 22:18:48 +0100

    feat: add route

---
+++ lib/core/navigation/route_names.dart
@@ -0,0 +1,104 @@
+class RouteNames {
+  static const String splash = '/';
+  static const String onBoarding = '/onBoarding';
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String otp = '/otp';
+  static const String success = '/success';
+  static const String terms = '/terms';
+  static const String privacy = '/privacy';
+  static const String featured = '/featured';
+  static const String restaurant = '/restaurant';
+  static const String supermarket = '/supermarket';
+  static const String pharmacy = '/pharmacy';
+  static const String hotelBooking = '/hotel-booking';
+  static const String promo = '/promo';
+  static const String restaurantItems = '/restaurantItems';
+  static const String pharmacyItems = '/pharmacyItems';
+  static const String supermarketItems = '/supermarketItems';
+  static const String search = '/search';
+  static const String searchService = '/searchService';
+  static const String searchAllService = '/searchAllService';
+  static const String productSearch = '/productSearch';
+  static const String shopDetails = '/shopDetails';
+  static const String restaurantRatings = '/restaurantRatings';
+  static const String pharmacyRatings = '/pharmacyRatings';
+  static const String supermarketRatings = '/supermarketRatings';
+  static const String restaurantCheckOut = '/restaurantCheckOut';
+  static const String pharmacyCheckOut = '/pharmacyCheckOut';
+  static const String supermarketCheckOut = '/supermarketCheckOut';
+  static const String payment = '/payment';
+  static const String onlinePayment = '/online-payment';
+  static const String payOnline = '/payOnline';
+  static const String carPayOnline = '/carPayOnline';
+  static const String orderDetails = '/orderDetails';
+  static const String orderTracking = '/orderTracking';
+  static const String orderDetailsPage = '/orderDetailsPage';
+  static const String orderCompletedPage = '/orderCompletedPage';
+  static const String updateDetails = '/updateDetails';
+  static const String enterOtp = '/enterOtp';
+  static const String support = '/support';
+  static const String ratings = '/ratings';
+  static const String faq = '/faq';
+  static const String legal = '/legal';
+  static const String feedback = '/feedback';
+  static const String referral = '/referral';
+  static const String delete = '/delete';
+  static const String wallet = '/wallet';
+  static const String addFunds = '/addFunds';
+  static const String addDebit = '/addDebit';
+  static const String transactions = '/transactions';
+  static const String hotelReservationsDetails = '/hotelReservationsDetails';
+  static const String carReservationsDetails = '/carReservationsDetails';
+  static const String barbingReservationsDetails = '/barbingReservationsDetails';
+  static const String hotelReservations = '/reservations';
+  static const String carReservations = '/carReservations';
+  static const String services = '/services';
+  static const String address = '/address';
+  static const String searchAddress = '/searchAddress';
+
+  static const String hotel = '/hotel';
+  static const String hotelPayment = '/hotelPayment';
+  static const String barbingServiceRequest = '/barbingServiceRequest';
+  static const String electricalServiceRequest = '/electricalServiceRequest';
+
+
+  //payment
+  static const String paymentWebView = '/payment_webview';
+
+  static const String tip = '/tip';
+  static const String hotelRooms = '/hotelRooms';
+  static const String carRental = '/carRentals';
+  static const String maintenance = '/maintenance';
+  static const String barbing = '/barbing';
+  static const String roomDetails = '/roomDetails';
+  static const String verifyAvailability = '/verifyAvailability';
+  static const String profileInfoDetails = '/profileInfoDetails';
+  static const String pickUpBooking = '/pickUpBooking';
+  static const String airportPickup = '/airportPickup';
+  static const String luxuryInfo = '/luxuryInfo';
+  static const String escortInfo = '/escortInfo';
+  static const String pickUpPayment = '/pickUpPayment';
+  static const String maintenancePayment = '/maintenancePayment';
+  static const String dropOffLocation = '/dropOffLocation';
+  static const String barbingBooking = '/barbingBooking';
+  static const String personalInfo = '/personalInfo';
+  static const String reservationInfo = '/reservationInfo';
+  static const String electricServiceBooking = '/electricServiceBooking';
+
+
+
+
+  //tabs
+  static const String cart = '/cart';
+
+
+
+
+
+
+
+
+
+}
